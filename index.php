9f4c41e28e7bc5a22c84e7833f8c05e99a2bd9ee
lazarev <k.lazarev@cleack.com>
2020-09-30 16:46:57 +0300

    Test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel='stylesheet'   href='/webpack_dist/bundle.css' media='all' />
-    <title>Document</title>
+    <title>777 - lottery</title>
 </head>
 <body>
     <?php include 'components/header/view.php'; ?>
